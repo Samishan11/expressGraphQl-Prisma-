5f5051b83b97eee962225100be4ad9ceb1b58a9b
Samishan11 <samishanthapa0@gmail.com>
2023-06-13 09:03:24 +0545

    socket setup

--- server.ts
+++ server.ts
@@ -1,9 +1,18 @@
 import express, { Express, Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client'
-
-const PORT = 5000
+import { Server } from "socket.io";
 const app: Express = express();
+const server = require('http').createServer(app);
+const io = new Server(server, {
+    cors: {
+      origin: "http://127.0.0.1:5173",
+      methods: ["GET", "POST", "DELETE", "PUT"],
+      credentials: true,
+    },
+  });
+const PORT = 5000
+const prisma = new PrismaClient()
 
-app.listen(PORT,()=>{
-    console.log(`server running at port ${PORT}`)
-})
+server.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+  });
