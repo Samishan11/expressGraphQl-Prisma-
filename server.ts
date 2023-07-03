3d2bfd5003d0a6b4de8c6a4ff5f73a1c5a1dd087
Samishan11 <samishanthapa0@gmail.com>
2023-07-03 15:45:38 +0545

    changes

--- server.ts
+++ server.ts
@@ -1,19 +1,43 @@
 import express, { Express, Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient();
 import { Server } from "socket.io";
 const app: Express = express();
 import { createServer } from 'http';
 const server = createServer(app)
 const io = new Server(server, {
-    cors: {
-      origin: "http://127.0.0.1:5173",
-      methods: ["GET", "POST", "DELETE", "PUT"],
-      credentials: true,
-    },
-  });
+  cors: {
+    origin: "http://127.0.0.1:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  },
+});
 const PORT = 5000
-const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.$connect();
+
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+  await prisma.user.create({
+    data:{
+      username:"Samishan",
+      email:"samishan@gmail.com",
+      password:"helloworld"
+    }
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  })
 
 server.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
-  });
+  console.log(`server running on port ${PORT}`);
+});
