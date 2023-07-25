9c672baedf3ddab43b9a305344e8a538efc71ad6
Samishan11 <samishanthapa0@gmail.com>
2023-07-25 22:25:20 +0545

    prisma db setup

--- server.ts
+++ server.ts
@@ -1,10 +1,10 @@
-import express, { Express, Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client'
+import express, { Express, Request, Response } from "express";
+import { PrismaClient, Prisma } from "@prisma/client";
 const prisma = new PrismaClient();
 import { Server } from "socket.io";
 const app: Express = express();
-import { createServer } from 'http';
-const server = createServer(app)
+import { createServer } from "http";
+const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://127.0.0.1:5173",
@@ -12,31 +12,19 @@ const io = new Server(server, {
     credentials: true,
   },
 });
-const PORT = 5000
+const PORT = 5000;
 
 async function main() {
-  await prisma.$connect();
-
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
   await prisma.user.create({
-    data:{
-      username:"Samishan",
-      email:"samishan@gmail.com",
-      password:"helloworld"
-    }
-  })
+    data: {
+      username: "Rich",
+      email: "hello@prisma.com",
+      password:'123456'
+    },
+  });
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  })
+main();
 
 server.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
